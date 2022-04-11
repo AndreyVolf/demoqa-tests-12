b876d821db1dc12afe54c2e8ab8ba18ddc832fec
Andrey <vtometall@gmail.com>
2022-04-11 14:13:39 +0300

    JUnit 5

---
+++ src/test/java/guru/qa/simpletest.java
@@ -0,0 +1,58 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class simpletest {
+
+    @BeforeAll
+     static  void  initDB () {
+        System.out.println("### @BeforeAll");
+
+    }
+
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###    @BeforeEach");
+ //       Selenide.open("http://ya.ru");
+    }
+
+    @AfterEach
+    void close() {
+        System.out.println("###    @AfterEach");
+//        WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static  void  cleanDB () {
+        System.out.println("### @AfterAll");
+
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###         @Test0");
+        //... Поиск в новостях
+    }
+
+    @Test
+    void assertTest2() {
+        System.out.println("###         @Test1");
+        //... Поиск в картинках
+    }
+
+    @Test
+    void assertTest3() {
+        System.out.println("###         @Test2");
+        //... Поиск в новостях
+    }
+
+    @Test
+    void assertTest4() {
+        System.out.println("###         @Test3");
+        //... Поиск в картинках
+    }
+}
+
